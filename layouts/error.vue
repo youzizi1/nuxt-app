6dd21d2d193d7e6a20112454bdff0b8c2da4ab37
youzizi1 <yinyun957@gmail.com>
2021-08-27 00:53:03 +0800

    init error page

---
+++ layouts/error.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    error page
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['error'],
+  layout: 'error_layout'
+}
+</script>
---
+++ layouts/error_layout.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>错误页面的布局</h1>
+    <Nuxt />
+  </div>
+</template>
