bd39be2e68b8f71c9dfd862794bbfe8cdb3eb815
youzizi1 <yinyun957@gmail.com>
2021-08-27 01:34:52 +0800

    add Tailwind CSS

--- pages/index.vue
+++ pages/index.vue
@@ -2,5 +2,18 @@
   <div>
     <p>首页</p>
     <img src="~/assets/images/crayon.jpeg" alt="crayon">
+    <div>
+      <button @click="onToast">toast</button>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    onToast() {
+      this.$toast.success('Successfully authenticated')
+    }
+  }
+}
+</script>
