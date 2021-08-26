c0683d8b91f31f0d591122e1dca7f0c4ad2cb3b6
youzizi1 <yinyun957@gmail.com>
2021-08-27 01:04:28 +0800

    single params

---
+++ pages/item/_id.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>item page</h1>
+    <div>item id: {{ $route.params.id }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  // 校验参数
+  validate({ params }) {
+    return /^\d+$/.test(params.id)
+  },
+}
+</script>
