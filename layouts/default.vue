f72e1c50f13b7ec1d222bf73fcd5144fadbbd44d
youzizi1 <yinyun957@gmail.com>
2021-08-27 00:37:23 +0800

    init default layout

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div>
+      <h1>公共头部</h1>
+      <div>
+        <a href="/">首页</a>
+        <a href="/about">关于</a>
+        <a href="/list">列表</a>
+        <a href="/list/detail">详情</a>
+      </div>
+    </div>
+    <Nuxt />
+  </div>
+</template>
